dbfc70ff0c0d534b421f4cd44a00b35073972f7c
hikmaa <106948962+hikma12@users.noreply.github.com>
2022-10-10 17:25:12 +0800

    Update DDL dan DML.sql

--- DDL dan DML.sql
+++ DDL dan DML.sql
@@ -31,8 +31,8 @@ foreign key (course_id) references courses (course_id)
 );
 desc schedule
 
-DML--
-masukkan data pada tabel students
+-- DML--
+-- masukkan data pada tabel students
 INSERT INTO students (`full name`, address, major)
 VALUE 
 ("Rendy Luis", "Jalan Sahabat", "Sistem Informasi"),
@@ -42,7 +42,7 @@ VALUE
 ("Andi Maghfirah Inzani S.", "BTP", "Sistem Informasi"),
 ("Nur Hikmah", "Jalan Sejati", "Sistem Informasi"),
 ("Martahan Mikhael Butar B.", "Aspol Perintis", "Sistem Informasi");
-select *from students;
+select * from students;
 
 INSERT INTO courses (name, duration)
 VALUE 
@@ -50,7 +50,7 @@ VALUE
 ("Pemrograman Web", 90),
 ("Struktur Diskrit", 90),
 ("Riset Teknologi", 150);
-#select *from Courses
+#select * from Courses
 
 insert into schedule (Student_id, course_id, date, time)
 value 
@@ -61,7 +61,7 @@ value
 (5, 3, '2022-10-13', '2022-10-13 16:00:00'),
 (6, 3, '2022-10-15', '2022-10-15 08:00:00'),
 (7, 1, '2022-10-19', '2022-10-19 08:00:00');
-select *from schedule
+select * from schedule
 
 update students
 set `full name`="Andi Maghfirah Inzani"
@@ -78,5 +78,3 @@ inner join schedule as Sc
 on St.student_id = Sc.student_id
 inner join courses as Co
 on Sc. course_id = Co.course_id
-
-
